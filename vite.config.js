83ae40a9320b7825cefa9abbd5346e739ea4ba47
chuangshinxin-bakery <jackchen950217@gmail.com>
2025-12-05 15:44:52 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // 重要：如果您 GitHub Repository 叫 "remote-camera"，這裡就要填 "/remote-camera/"
-  base: '/remote-camera/', 
-})
